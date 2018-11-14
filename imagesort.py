01011b3327beeee3e74b872e34fa398833929f74
dreamxinxcode <42306596+dreamxinxcode@users.noreply.github.com>
2018-11-13 17:53:51 -0800

    Add files via upload

--- imagesort.py
+++ imagesort.py
@@ -1,14 +1,26 @@
 import os
 import shutil
 
-source="/home/gentoo/Downloads"
-#input("Enter your source directory: ")
-imageDestination="/home/gentoo/Pictures"
-videoDestination="/home/gentoo/Videos"
+print(".___                                _________              __")                    
+print("|   | _____ _____     ____   ____  /   _____/ ____________/  |_    ______ ___.__.")
+print("|   |/      \\__  \   / ___\_/ __ \ \_____  \ /  _ \_  __ \   __\   \____ <   |  |")
+print("|   |  Y Y  \/ __ \_/ /_/  >  ___/ /        (  <_> )  | \/|  |     |  |_> >___  |")
+print("|___|__|_|  (____  /\___  / \___  >_______  /\____/|__|   |__| /\  |   __// ____|")
+print("          \/     \//_____/      \/        \/                   \/  |__|   \/")     
+print("                                                     Developed by: @dream.in.code")                        
+
+#source="/home/gentoo/Downloads"
+source=input("Enter your source directory: ")
+#imageDestination="/home/gentoo/Pictures"
+imageDestination=input("Enter your image destination path: ")
+#videoDestination="/home/gentoo/Videos"
+videoDestination=input("Enter your video destination path: ")
 files = os.listdir(source)
 
 for file in files:
     if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png") or file.endswith(".gif"):
         shutil.move(os.path.join(source, file), os.path.join(imageDestination, file))
-    if file.endswith(".mp4") or file.endswith(".avi") or file.endswith(".flv") or file.endswith(".wmv") or file.endswith(".mov"):
+    if file.endswith(".mp4") or file.endswith(".avi") or file.endswith(".flv") or file.endswith(".webm") or file.endswith(".wmv") or file.endswith(".mov"):
         shutil.move(os.path.join(source, file), os.path.join(videoDestination, file))
+print("moving...")
+print("Done")
